55ded0af96f9637ae1a67ca65b50c56ffe1995b4
tjjjia <14137521+tjjjia@users.noreply.github.com>
2025-09-10 12:35:29 +0200

    photoswipe++

--- src/lib/components/blocks/BlockGallery.svelte
+++ src/lib/components/blocks/BlockGallery.svelte
@@ -4,7 +4,6 @@
 	https://getkirby.com/docs/reference/panel/blocks/gallery
 
 	*/
-
 	export let block = {
 		content: [],
 		id: "",
@@ -13,14 +12,78 @@
 	};
 
 	const images = block.content;
+	const crop = images[0].crop ?? false;
+	const ratio = images[0].ratio ?? "auto";
+
+	/* Photoswipe code
+	https://photoswipe.com/getting-started/
+
+	*/
+	import { onMount } from "svelte";
+	import PhotoSwipeLightbox from "photoswipe/lightbox";
+	import "photoswipe/style.css";
+
+	onMount(() => {
+		const lightbox = new PhotoSwipeLightbox({
+			gallery: ".pswp-gallery",
+			children: ".pswp-gallery--item",
+			pswpModule: () => import("photoswipe")
+		});
+		lightbox.on("uiRegister", function () {
+			lightbox.pswp.ui.registerElement({
+				name: "custom-caption annotation medium",
+				order: 9,
+				isButton: false,
+				appendTo: "root",
+				html: "Caption text",
+				onInit: (el, pswp) => {
+					lightbox.pswp.on("change", () => {
+						const currSlideElement = lightbox.pswp.currSlide.data.element;
+						let captionHTML = "";
+						if (currSlideElement) {
+							const hiddenCaption = currSlideElement.querySelector(".hidden-caption-content");
+							if (hiddenCaption) {
+								// get caption from element with class hidden-caption-content
+								captionHTML = hiddenCaption.innerHTML;
+							} else {
+								// get caption from alt attribute
+								captionHTML = currSlideElement.querySelector("img").getAttribute("data-caption");
+							}
+						}
+						el.innerHTML = captionHTML || "";
+					});
+				}
+			});
+		});
+
+		lightbox.init();
+	});
 </script>
 
 {#if !block.isHidden}
-	<figure>
-		<ul>
+	<figure data-ratio={ratio} data-crop={crop}>
+		<div class="auto-cols pswp-gallery">
 			{#each images as image}
-				<li><img src={image.src} alt={image.alt} srcset={image.srcset} /></li>
+				<div class="pswp-gallery--item">
+					<a
+						href={image.src}
+						data-pswp-width={image.width}
+						data-pswp-height={image.height}
+						target="_blank"
+					>
+						<img
+							alt={image.alt}
+							data-caption={image.caption}
+							data-sizes="auto"
+							src={image.src}
+							srcset={image.srcset}
+							data-srcset={image.srcset}
+							width={image.width}
+							height={image.height}
+						/>
+					</a>
+				</div>
 			{/each}
-		</ul>
+		</div>
 	</figure>
 {/if}
--- src/lib/components/blocks/BlockImage.svelte
+++ src/lib/components/blocks/BlockImage.svelte
@@ -9,9 +9,9 @@
 		content: {
 			alt: "",
 			caption: "",
-			crop: "",
+			crop: false,
 			link: "",
-			ratio: "",
+			ratio: "auto",
 			src: null,
 			srcset: null
 		},
@@ -20,18 +20,79 @@
 		type: "image"
 	};
 
-	const { alt, caption, crop, link, ratio, src, srcset } = block.content;
+	const { alt, caption, crop, height, link, ratio, src, srcset, width } = block.content;
+
+
+	/* Photoswipe code
+	https://photoswipe.com/getting-started/
+
+	*/
+	import { onMount } from "svelte";
+	import PhotoSwipeLightbox from "photoswipe/lightbox";
+	import "photoswipe/style.css";
+
+	onMount(() => {
+		const lightbox = new PhotoSwipeLightbox({
+			gallery: ".pswp-gallery",
+			children: ".pswp-gallery--item",
+			pswpModule: () => import("photoswipe")
+		});
+		lightbox.on("uiRegister", function () {
+			lightbox.pswp.ui.registerElement({
+				name: "custom-caption annotation medium",
+				order: 9,
+				isButton: false,
+				appendTo: "root",
+				html: "Caption text",
+				onInit: (el, pswp) => {
+					lightbox.pswp.on("change", () => {
+						const currSlideElement = lightbox.pswp.currSlide.data.element;
+						let captionHTML = "";
+						if (currSlideElement) {
+							const hiddenCaption = currSlideElement.querySelector(".hidden-caption-content");
+							if (hiddenCaption) {
+								// get caption from element with class hidden-caption-content
+								captionHTML = hiddenCaption.innerHTML;
+							} else {
+								// get caption from alt attribute
+								captionHTML = currSlideElement.querySelector("img").getAttribute("data-caption");
+							}
+						}
+						el.innerHTML = captionHTML || "";
+					});
+				}
+			});
+		});
+
+		lightbox.init();
+	});
 </script>
 
 {#if !block.isHidden}
 	<section class="block">
-		<figure>
+		<figure data-ratio={ratio} data-crop={crop}>
 			{#if link}
-				<a href={link} target="_blank">
-					<img {src} {alt} {srcset} />
-				</a>
+				<div class="link-external">
+					<a href={link} target="_blank">
+						<img {src} {alt} {srcset} />
+					</a>
+				</div>
 			{:else}
-				<img {src} {alt} {srcset} />
+				<div class="auto-cols pswp-gallery">
+					<div class="pswp-gallery--item">
+						<a href={src} data-pswp-width={width} data-pswp-height={height} target="_blank">
+							<img
+								{alt}
+								data-caption={caption}
+								data-sizes="auto"
+								{src}
+								{srcset}
+								data-srcset={srcset}
+								{width}
+								{height} />
+						</a>
+					</div>
+				</div>
 			{/if}
 
 			{#if caption}
